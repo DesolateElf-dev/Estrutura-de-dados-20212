9b77f08cc7a0a3ecdc81fbd2ceb423f703ac225c
DesolateElf-dev <54561697+DesolateElf-dev@users.noreply.github.com>
2021-08-23 20:59:24 -0300

    update

---
+++ Ex01/controller/SomaNaturais.java
@@ -0,0 +1,20 @@
+package controller;
+
+public class SomaNaturais {
+
+	public SomaNaturais() {
+		super();
+	}
+	
+	public int FazSoma(int n) {
+		
+		int soma=0;
+		
+		
+		
+		
+		return soma;
+	}
+	
+
+}
--- Ex01/view/Principal.java
+++ Ex01/view/Principal.java
@@ -2,22 +2,69 @@ package view;
 
 import javax.swing.JOptionPane;
 
-import controller.InverteTexto;
+import controller.*;
 
 public class Principal {
 
 	public static void main(String[] args) {
+		
+		while(true) {
 			
-		String texto = JOptionPane.showInputDialog("Digite o texto a ser invertido: ");
+			int opc = Integer.parseInt(JOptionPane.showInputDialog(null,"Escolha o programa a ser executado"
+					+ "\n1: Ex 1.1- Calcula fatorial"
+					+ "\n2: Ex 1.2- Inverte um texto"
+					+ "\n3: Ex 2.1- Soma sequencia de números naturais"
+					+ "\n4: Ex 2.2- Checa caracteres de um número"
+					+ "\n0: Finalizar"));			
 		
-		InverteTexto vc = new InverteTexto();
-		
-		int tamanho = texto.length();
-		
-		String otxet = vc.Inversor(texto, tamanho);
-		
-		System.out.println(otxet);
-
+			switch (opc){
+				case 1: CalcFatView();
+					break;
+				case 2: InverteTextoView();
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 0:
+					System.exit(0);
+				default: JOptionPane.showMessageDialog(null,"Inválido",
+					      "Erro!", JOptionPane.ERROR_MESSAGE);
+				
+			}
+		}
 	}
+	
+		
+		static void CalcFatView(){
+			
+			int n = Integer.parseInt(JOptionPane.showInputDialog("Digite número: "));
+			
+			CalcFatorial f = new CalcFatorial();
+			
+			Double fatorial = f.CalcFat(n);
+			
+			System.out.println(fatorial);
+		}
+		
+		static void InverteTextoView() {
+
+			String texto = JOptionPane.showInputDialog("Digite o texto a ser invertido: ");
+			
+			InverteTexto vc = new InverteTexto();
+			
+			int tamanho = texto.length();
+			
+			String otxet = vc.Inversor(texto, tamanho);
+			
+			System.out.println(otxet);
+		}
+		
+		static void SomaNaturaisView() {
+			
+		}
+
+
+
 
 }
