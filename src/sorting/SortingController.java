3d564c92dc5bbc76fb7af820a39657e2f7d690fa
DesolateElf-dev <54561697+DesolateElf-dev@users.noreply.github.com>
2021-08-28 12:50:51 -0300

    Corrigindo nomes

--- src/sorting/SortingController.java
+++ src/sorting/SortingController.java
@@ -3,7 +3,48 @@ package sorting;
 public class SortingController {
 
 	public SortingController() {
-		// TODO Auto-generated constructor stub
+	}
+	
+	public int[] mergeSort(int[] array, int start, int end){
+		
+		System.out.println("Vetor da posição "+start+" até "+end);
+		
+		if (start<end) {
+			int middle = (start+end)/2;
+			mergeSort(array, start, end); //Left
+			mergeSort(array, middle+1, end); //Right
+			merge(array, start, middle, end);
+			
+		}
+		return array;
+		
+	}
+
+	private void merge(int[] array, int start, int middle, int end) {
+		int newArray[] = new int[array.length];
+		
+		for (int i=start; i<=end;i++) {
+			newArray[i]=array[i];
+		}
+		int left=start;
+		int right=middle+1;
+		
+		for(int count=start;count<=end;count++) {
+			if (left<middle) {
+				array[count] = newArray[right];
+				right++;
+			} else if (right>end) {
+				array[count] = newArray[left];
+				left++;
+			} else if (newArray[left]<newArray[right]) {
+				array[count] = newArray[left];
+				left++;
+			} else {
+				array[count] = newArray[right];
+				right++;
+			}
+		}
+		
 	}
 
 }
